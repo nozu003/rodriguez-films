cf8c457324d6c0218f8a96f7ae55bc23dacae84c
nozu003 <81842374+nozu003@users.noreply.github.com>
2021-09-06 15:34:39 +0800

    Quick update
    Added the functionality of image viewing for the gallery section.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
+
+  const images = ["assets/images/gallery/landscape/20210817_074845.jpg","assets/images/gallery/landscape/aaaaa.jpg","assets/images/gallery/landscape/IMG_0662.jpg","assets/images/gallery/landscape/IMG_1224.jpg","assets/images/gallery/landscape/IMG_3748.jpg","assets/images/gallery/landscape/IMG_5613.jpg","assets/images/gallery/landscape/IMG_8093-2.jpg","assets/images/gallery/landscape/IMG_9590.jpg","assets/images/gallery/landscape/IMG_9971.jpg","assets/images/gallery/landscape/LRM_EXPORT_20180420_182024.jpg","assets/images/gallery/landscape/LRM_EXPORT_20180422_183447.jpg","assets/images/gallery/landscape/WallpaperX.jpg","assets/images/gallery/landscape/Wide.jpg","assets/images/gallery/landscape/1_Thumbnail.jpg"];
+  
     if(document.title == "Rodriguez Films | Home"){
         var counter = 1;
     setInterval(function(){
@@ -17,7 +20,107 @@ $(document).ready(function(){
       });
     }
 
+    // GALLERY
+    if(document.title == "Rodriguez Films | Gallery")
       $('.categ-1').click(function(){
         location.href = "/landscape.html";
+      });
+
+    // LANDSCAPE
+    if(document.title == "Rodriguez Films | Landscape")
+    // CATEG 1
+      $('.categ-1').click(function(){
+        $('.display-box').removeClass('d-none');
+        $('body').addClass('overflow-hidden');
+        document.getElementById('image-display').src = images[0];
+      });
+      // CATEG 2
+      $('.categ-2').click(function(){
+        $('.display-box').removeClass('d-none');
+        $('body').addClass('overflow-hidden');
+        document.getElementById('image-display').src = images[1];
+      });
+      // CATEG 3
+      $('.categ-3').click(function(){
+        $('.display-box').removeClass('d-none');
+        $('body').addClass('overflow-hidden');
+        document.getElementById('image-display').src = images[2];
+      });
+      // CATEG 4
+      $('.categ-4').click(function(){
+        $('.display-box').removeClass('d-none');
+        $('body').addClass('overflow-hidden');
+        document.getElementById('image-display').src = images[3];
+      });
+      // CATEG 5
+      $('.categ-5').click(function(){
+        $('.display-box').removeClass('d-none');
+        $('body').addClass('overflow-hidden');
+        document.getElementById('image-display').src = images[4];
+      });
+      // CATEG 6
+      $('.categ-6').click(function(){
+        $('.display-box').removeClass('d-none');
+        $('body').addClass('overflow-hidden');
+        document.getElementById('image-display').src = images[5];
+      });
+      // CATEG 7
+      $('.categ-7').click(function(){
+        $('.display-box').removeClass('d-none');
+        $('body').addClass('overflow-hidden');
+        document.getElementById('image-display').src = images[6];
+      });
+      // CATEG 8
+      $('.categ-8').click(function(){
+        $('.display-box').removeClass('d-none');
+        $('body').addClass('overflow-hidden');
+        document.getElementById('image-display').src = images[7];
+      });
+      // CATEG 9
+      $('.categ-9').click(function(){
+        $('.display-box').removeClass('d-none');
+        $('body').addClass('overflow-hidden');
+        document.getElementById('image-display').src = images[8];
+      });
+      // CATEG 10
+      $('.categ-10').click(function(){
+        $('.display-box').removeClass('d-none');
+        $('body').addClass('overflow-hidden');
+        document.getElementById('image-display').src = images[9];
+      });
+      // CATEG 11
+      $('.categ-11').click(function(){
+        $('.display-box').removeClass('d-none');
+        $('body').addClass('overflow-hidden');
+        document.getElementById('image-display').src = images[10];
+      });
+      // CATEG 12
+      $('.categ-12').click(function(){
+        $('.display-box').removeClass('d-none');
+        $('body').addClass('overflow-hidden');
+        document.getElementById('image-display').src = images[11];
+      });
+      // CATEG 13
+      $('.categ-13').click(function(){
+        $('.display-box').removeClass('d-none');
+        $('body').addClass('overflow-hidden');
+        document.getElementById('image-display').src = images[12];
+      });
+      // CATEG 14
+      $('.categ-14').click(function(){
+        $('.display-box').removeClass('d-none');
+        $('body').addClass('overflow-hidden');
+        document.getElementById('image-display').src = images[13];
+      });
+      // CATEG 15
+      $('.categ-15').click(function(){
+        $('.display-box').removeClass('d-none');
+        $('body').addClass('overflow-hidden');
+        document.getElementById('image-display').src = images[14];
+      });
+    // D-BOX
+      $('.display-bg').click(function(){
+        $('.display-box').addClass('d-none');
+        $('body').removeClass('overflow-hidden');
       });
 });
